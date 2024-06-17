37fd43fccd80f30c00b0fcc322cf0888d198ae83
韩喆鑫 <hanzhexin@hanzhexindeMacBook-Pro.local>
2024-06-17 14:14:56 +0800

    update

--- util/batcher/equal_len/batcher_p.py
+++ util/batcher/equal_len/batcher_p.py
@@ -1,7 +1,5 @@
 #coding=utf-8
 from util.BatchData import batch_all
-from util.Formater import add_pad
-from util.Bitmap import bitmap_by_padid
 import numpy as np
 import copy
 import random
@@ -60,15 +58,6 @@ class batcher(object):
         self.in_tags = []
         self.out_tags = []
 
-        self.in_duration_ms = []
-        self.out_duration_ms = []
-
-        self.in_play_time_ms = []
-        self.out_play_time_ms = []
-
-        self.is_click_in_list= []
-        self.is_click_out_list= []
-
         samplelist = self.len_dic[self.key_list[self.rand_idx[self.idx]]]
         random.shuffle(samplelist)
         for sample in samplelist:
@@ -77,14 +66,6 @@ class batcher(object):
             self.out_idxes.append(sample.out_idxes)
             self.in_tags.append(sample.in_tag)
             self.out_tags.append(sample.out_tag)
-            # self.in_duration_ms.append(sample.in_duration_ms)
-            # self.out_duration_ms.append(sample.out_duration_ms)
-            # self.in_play_time_ms.append(sample.in_play_time_ms)
-            # self.out_play_time_ms.append(sample.out_play_time_ms)
-            # self.is_click_in_list.append(sample.is_click_in_list)
-            # self.is_click_out_list.append(sample.is_click_out_list)
-            # self.in_duration_ms,self.out_duration_ms,self.in_play_time_ms,self.out_play_time_ms,self.is_click_in_list,self.is_click_out_list
-            # ,self.in_tags,self.out_tags
         rins, lab, rinlens, rmaxlens, rinlens_float32 = batch_all(
             [self.in_idxes,self.out_idxes,self.in_tags,self.out_tags]
         )
@@ -93,14 +74,6 @@ class batcher(object):
         out_idxes = rins[1]
         in_tags = rins[2]
         out_tags = rins[3]
-        # in_duration_ms = rins[2]
-        # out_duration_ms = rins[3]
-        # in_play_time_ms = rins[4]
-        # out_play_time_ms = rins[5]
-        # is_click_in_list = rins[6]
-        # is_click_out_list = rins[7]
-
-
 
         # context bitmap.
         sent_bitmap = []
@@ -117,12 +90,6 @@ class batcher(object):
             'out_idxes': out_idxes,
             'in_tags':  in_tags,
             'out_tags': out_tags,
-            # 'in_duration_ms':in_duration_ms,
-            # 'out_duration_ms':out_duration_ms,
-            # 'in_play_time_ms':in_play_time_ms,
-            # 'out_play_time_ms':out_play_time_ms,
-            # 'is_click_in_list':is_click_in_list,
-            # 'is_click_out_list':is_click_out_list,
             'seq_lens': seq_lens
         }
         return ret_data
